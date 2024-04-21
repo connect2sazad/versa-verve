d09313e1df48bc83b773953bb0482977399be375
Sazad Ahemad <mail2sazad@gmail.com>
2024-04-21 09:37:24 +0530

    added backend springboot base

---
+++ backend/src/main/java/com/versaverve/versaverve/VersaverveApplication.java
@@ -0,0 +1,13 @@
+package com.versaverve.versaverve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersaverveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersaverveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/versaverve/versaverve/VersaverveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.versaverve.versaverve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersaverveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
